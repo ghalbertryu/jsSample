c3e1041a84872eb3dddb49186acc3473c0620a9a
AlbertRyu <albert.liu@flowring.com>
2019-10-25 17:51:40 +0800

    Fix filename content "(", ")" or "." characters bug

--- src/renameDuplicateFile.js
+++ src/renameDuplicateFile.js
@@ -1,20 +1,19 @@
-var fileName = "image.jpg";
-var fileNames = ["image.jpg", "image(1).jpg"];
+var fileName = "image(2).jpg";
+var fileNames = ["image(2).jpg", "image(2)(1).jpg"];
 
 var fileName = findFileNameWithoutDuplicate(fileName, fileNames);
 console.log(fileName);
 
-
 function findFileNameWithoutDuplicate (fileName, fileNames) {
     if (fileNames.some((el, idx, arr) => el.toUpperCase() == fileName.toUpperCase())) {
         var fileNamePrefix = fileName.substring(0, fileName.lastIndexOf('.'));
-        var fileNameSuffix = fileName.substring(fileName.lastIndexOf('.'), fileName.length );
-        var regExp = new RegExp(fileNamePrefix + "\\(\\d+\\)" + fileNameSuffix, "");
+        var fileNameSuffix = fileName.substring(fileName.lastIndexOf('.'), fileName.length);
+        var regExp = new RegExp(addEscapeCharAbove(fileNamePrefix) + "\\(\\d+\\)" + addEscapeCharAbove(fileNameSuffix));
         var numbersInArray = fileNames.filter((el) => regExp.test(el))
-                                    .map((el) => el.replace(new RegExp(fileNamePrefix + "\\(", ""), ""))
-                                    .map((el) => el.replace(new RegExp("\\)" + fileNameSuffix, ""), ""))
+                                    .map((el) => el.replace(new RegExp(addEscapeCharAbove(fileNamePrefix) + "\\("), ""))
+                                    .map((el) => el.replace(new RegExp("\\)" + addEscapeCharAbove(fileNameSuffix)), ""))
                                     .map((el) => Number.parseInt(el));
-        var maxNumberInArray = Math.max(0, numbersInArray);
+        var maxNumberInArray = Math.max(0, ...numbersInArray);
         // console.log(maxNumberInArray);
     
         fileName = fileNamePrefix + 
@@ -22,4 +21,8 @@ function findFileNameWithoutDuplicate (fileName, fileNames) {
                     fileNameSuffix
     }
     return fileName;
+}
+
+function addEscapeCharAbove(str) {
+    return str.replace(/([\(\)\.])/g, "\\$1");
 }
